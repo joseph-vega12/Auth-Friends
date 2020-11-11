ba713613f26c98afb683b44f4b329aaeadd49914
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-11 14:47:40 -0800

    implemented update where user is allowed to add new friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,10 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+const initialFormState = {
+    name: '',
+    age: '',
+    email: '',
+}
+
 
 function Friends() {
 
-    const [state, setState] = useState([]);
+    const [friendstate, setFriendState] = useState([]);
+    const [formValue, setFormValue] = useState(initialFormState);
+
+    const handleChange = (e) => {
+        setFormValue({
+            ...formValue,
+            [e.target.name]: e.target.value
+        })
+    }
 
     useEffect(() => {
         getFriends();
@@ -15,20 +29,52 @@ function Friends() {
         axiosWithAuth()
             .get("http://localhost:5000/api/friends")
             .then(res => {
-                setState(res.data);
+                setFriendState(res.data);
                 console.log(res.data);
             })
             .catch(err => {
                 console.log(err);
             })
     }
-
+    const handlePost = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post("http://localhost:5000/api/friends", formValue)
+            .then(res => {
+                setFriendState(res.data);
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
     return (
-        <div>
-            <h1>You Made It!!</h1>
-            {state.map(friends =>
-                <div key={friends.id}>
-                    <h4>{friends.name}</h4>
+        <div className="container">
+            <div className="friend-form">
+                <h1>Create a New Friend:</h1>
+                <form>
+                    <ul>
+                        <label>
+                            Name:
+                            <input name="name" type="text" onChange={handleChange} />
+                        </label>
+                        <label>
+                            Email:
+                            <input name="email" type="email" onChange={handleChange} />
+                        </label>
+                        <label>
+                            Age:
+                            <input name="age" type="number" onChange={handleChange} />
+                        </label>
+                        <button onClick={handlePost}>Submit</button>
+                    </ul>
+                </form>
+            </div>
+            {friendstate.map(friends =>
+                <div className="friends-cards" key={friends.id}>
+                    <h2>{friends.name}</h2>
+                    <h4>{friends.email}</h4>
+                    <h4>{friends.age}</h4>
                 </div>
             )}
         </div>
